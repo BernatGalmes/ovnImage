d3f758b933b537455d5a4df9e07c653f33b15150
Bernat <bernat_galmes@hotmail.com>
2019-04-09 18:14:46 +0200

    added plots as package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='ovnImage',
-      version='0.2.1',
+      version='0.2.2',
       description='Useful helper functions of image processing and machine learning',
       url='',
       author="Miquel Miró Nicolau, Bernat Galmés Rubert, Dr. Gabriel Moyà Alcover",
       author_email='miquelca32@gmail.com, bernat_galmes@hotmail.com, gabriel_moya@uib.es',
       license='MIT',
-      packages=['ovnImage'],
+      packages=['ovnImage', 'ovnImage.plots'],
       install_requires=[
           'scikit-learn',
           'matplotlib',
